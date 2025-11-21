0d5225a7caf2a6b55162599d0c3b00325d77da83
Hal <hal.spidernight@gmail.com>
2025-11-21 18:33:21 +0900

    feat: connect nuxt and hono by rpc

--- sample/hono/backend/src/index.ts
+++ sample/hono/backend/src/index.ts
@@ -1,15 +1,59 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+import { cors } from 'hono/cors'
+import { zValidator } from '@hono/zod-validator'
+import { z } from 'zod'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(logger())
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.use(
+  '/api/*',
+  cors({
+    origin: '*',
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  })
+)
+
+const routes = app
+  .get('/api', (c) => {
+    return c.text('Hello Hono!')
+  })
+  .post(
+    '/api/users',
+    zValidator(
+      'json',
+      z.object({
+        name: z.string().min(2),
+        email: z.string(),
+        age: z.coerce.number().min(0).optional(),
+      })
+    ),
+    async (c) => {
+      const { name, email, age } = c.req.valid('json')
+
+      const user = {
+        id: crypto.randomUUID(),
+        name,
+        email,
+        age,
+        createdAt: new Date().toISOString(),
+      }
+
+      return c.json(user, 201)
+    }
+  )
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3001,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`)
+  }
+)
+
+export type AppType = typeof routes
--- sample/hono/frontend/app/app.vue
+++ sample/hono/frontend/app/app.vue
@@ -4,3 +4,21 @@
     <NuxtWelcome />
   </div>
 </template>
+<script setup lang="ts">
+import { hc } from 'hono/client'
+import type { AppType } from '../../backend/src/index'
+
+const client = hc<AppType>('http://localhost:3001/')
+
+client.api.users
+  .$post({
+    json: {
+      name: 'Taro Yamada',
+      email: 'sssssss@gmail.com',
+      age: 25,
+    },
+  })
+  .then((res) => {
+    console.log(res)
+  })
+</script>
